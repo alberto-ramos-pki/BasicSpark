afd012369d06c6dc1c86cd682a56c40935364ae7
alberto-ramos-pki <alberto.ramos@perkinelmer.com>
2021-10-28 15:35:43 +0200

    testing scala import class

---
+++ src/main/scala/com/perkin/scala/test_scala_object.scala
@@ -0,0 +1,5 @@
+package com.perkin.scala
+
+object test_scala_object {
+
+}
---
+++ src/main/scala/com/perkin/scala/val_with_double_dot.scala
@@ -0,0 +1,12 @@
+package com.perkin.scala
+
+object val_with_double_dot {
+
+  val test_array :: other = List(1,2,4)
+
+  def main(args: Array[String]): Unit = {
+    println("ee")
+
+  }
+
+}
